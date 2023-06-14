ee71c7ffc33c6723b7ea0a7fe26a7b4fb6a51b91
kirtikumarV2 <kirtikumar@vasyerp.com>
2023-06-14 12:01:08 +0530

    First Commit

---
+++ app/src/main/java/com/example/navexample/Fragment_1.kt
@@ -0,0 +1,58 @@
+package com.example.navexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class Fragment_1 : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view: View = inflater.inflate(R.layout.fragment_1, container, false)
+
+        // create instance of navigate button
+        val navigateButton: Button = view.findViewById(R.id.navigate_button)
+
+        // handle the navigate button to navigate to the fragment 2
+        navigateButton.setOnClickListener {
+            findNavController().navigate(R.id.action_fragment_1_to_fragment_2)
+        }
+
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Fragment_1().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/navexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.navexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.navexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+    }
+}
